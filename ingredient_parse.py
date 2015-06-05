550bcf6b50fe8f2f66f66795edc385923fdeb87e
Max Candocia <mscandocia@gmail.com>
2015-06-04 22:32:49 -0500

    adding ingredient-parsing file (started)

---
+++ ingredient_parse.py
@@ -0,0 +1,21 @@
+test_cases = ['2 cups canned garbanzo beans, drained','2 cloves garlic, halved',
+	      '1/3 cup tahini','1/4 cup lemon juice','1 teaspoon salt',
+	      '1 pinch paprika','1 tablespoon olive oil','1 teaspoon minced fresh parsely',
+	      '1 clove garlic','1 (19 ounce) can garbanzo beans, half the liquid reserved',
+	      '4 tablespoons lemon juice','1 cup garbanzo beans','1/3 cup canned jalepeno pepper slices, juice reserved',
+	      '1/2 teapsoon curry powder','1/2 teaspoon ground cumin','crushed red pepper to taste',
+	      '1 (15.5 ounce) can garbanzo beans (chickpeas), drained','1/3 cup pitted Spanish Manzanilla olives',
+	      '1 1/2 teaspoons chopped fresh basil','salt and pepper to taste',
+	      '1 teaspoon cilantro leaves','1 cup cooked chickpeas',
+	      '2 cloves garlic, finely chopped','3/4 teaspoon ground cumin',
+	      '1/4 cup water','2 tablespoons chopped fresh parsely, or to taste',
+	      '1/2 cup tahini','2 (15 ounce) cans garbanzo beans, drained',
+	      '1/4 cup crumbled feta cheese','1/4 cup sweet chili sauce',
+	      '1 tablespoon water, or as needed (optional)','15 pitted kalamata olives',
+	      '2 teaspoons dried dill weed','1 cup non-fat cottage cheese',
+	      '1 pint grape tomatoes, coarsely chopped','1 (15.5 ounce) can cannellini beans',
+	      '1 bunch fresh basil, chopped','ground black pepper to taste',
+	      '1 teaspoon grated lemon zest, minced']
+
+def parse_ingredient(text):
+	pass
