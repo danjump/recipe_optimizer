adc08608748b69967e39a0f8e26596d849275d35
Max Candocia <mscandocia@gmail.com>
2015-07-22 18:15:18 -0500

    added some more robustness to queries

--- analyze_data/linear_modprog_solution.py
+++ analyze_data/linear_modprog_solution.py
@@ -16,22 +16,32 @@ import sys
 def main(args):
 	data = read_data('hummus')
 	
-def read_data(query):
+#may want to move function to other file later
+def read_data(query,cutoff = 5):
 	"""reads in the data from the sql database
-	technically not safe from injections because of 
-	the query, but as long as you don't do anything like
-	query = 'DROP TABLE ingredients;DROP TABLE recipes;',
-	we should be fine
+	note that recipe ID and ingredient ID are set from ranges 
+	[0, k] and [0,N], where k and N are the number of distinct
+	ingredients and recipes (after making cuts to arcane ingredients)
 	"""
+	#
+	if type(query) <> type('hi'):
+		print "QUERY NEEDS TO BE A STRING"
+		raise TypeError
+	if type(cutoff) <> type(1):
+		print "CUTOFF NEEDS TO BE AN INTEGER"
+		raise TypeError
+	if re.search('[^a-zA-Z0-9 ]',query) <> None:
+		print 'INVALID QUERY, POSSIBLE INJECTION ATTEMPT'
+		raise 
 	os.chdir('/home/max/workspace/recipe_optimizer')
 	conn = sqlite3.connect('recipes.db')
 	c = conn.cursor()
 	c.execute("""CREATE TEMP TABLE temptable AS SELECT 
-		fr.recipe_id,ingredient_id, amount,
+		fr.recipe_id AS recipe_id,ingredient_id, amount,
 		yield_quantity,description,-1 AS idx
 		FROM (SELECT 
 		CASE
-		WHEN summary.total < 5 THEN 0
+		WHEN summary.total < %s THEN 0
 		ELSE ingredients.ingredient_id
 		END AS ingredient_id,
 		recipe_id, description,amount
@@ -44,19 +54,46 @@ def read_data(query):
 		ORDER BY count(*)) summary
 		WHERE summary.ingredient_id = ingredients.ingredient_id) fr
 		INNER JOIN (SELECT 
-		recipe_id,sum(amount) AS yield_quantity FROM ingredients
+		recipe_id,SUM(amount) AS yield_quantity FROM ingredients
 		GROUP BY recipe_id) sr
-		ON fr.recipe_id = sr.recipe_id;""" % (query))
+		ON fr.recipe_id = sr.recipe_id;""" % (cutoff,query))
 	c.execute("""UPDATE temptable SET idx=
 		(SELECT count(*) FROM 
 		(SELECT DISTINCT ingredient_id AS ingredient_id
 		FROM temptable AS tt) tt2
 		WHERE temptable.ingredient_id > tt2.ingredient_id
 		);""")
-	c.execute("""SELECT * FROM temptable;""")
+	c.execute("""UPDATE temptable SET recipe_id=
+		(SELECT count(*) FROM 
+		(SELECT DISTINCT recipe_id AS recipe_id
+		FROM temptable AS tt) tt2
+		WHERE temptable.recipe_id > tt2.recipe_id
+		);""")
+	c.execute("""SELECT recipe_id,ingredient_id,amount,yield_quantity
+		, description, idx FROM temptable;""")
 	data = c.fetchall()
 	c.close()
 	conn.close()
+	return data
+
+def keep_data_numbers(data):
+	data = [[x[0],x[5],x[2]/x[3]] for x in data]
+	#data = numpy.asarray(data)
+	return data
+
+def create_model_frame(data):
+	k = max([x[1] for x in data])
+	N = max([x[0] for x in data])
+	new_array = numpy.zeros([N,k])
+	print "NEED TO FINISH WRITING FUNCTION"
+
+def extract_ingredient_labels(data):
+	pass
+
+#database scraping needs to be updated before this function can be used
+#currently recipe names are not used
+def extract_recipe_labels(data):
+	pass
 
 if __name__=='__main__':
 	main(sys.argv[1:])
