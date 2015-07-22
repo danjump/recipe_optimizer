0929305af04fc5c6be3bf398a35d04c42d99be96
Max Candocia <mscandocia@gmail.com>
2015-07-22 16:00:15 -0500

    changed folder of linear_modprog_solution.py and finished sql queries for it

--- analyze_data/linear_modprog_solution.py
+++ analyze_data/linear_modprog_solution.py
@@ -14,29 +14,49 @@ import sys
 #output this new table to python
 
 def main(args):
+	data = read_data('hummus')
+	
+def read_data(query):
+	"""reads in the data from the sql database
+	technically not safe from injections because of 
+	the query, but as long as you don't do anything like
+	query = 'DROP TABLE ingredients;DROP TABLE recipes;',
+	we should be fine
+	"""
 	os.chdir('/home/max/workspace/recipe_optimizer')
 	conn = sqlite3.connect('recipes.db')
 	c = conn.cursor()
-	c.execute('CREATE TABLE temp_table
-		(ingredient_id INTEGER,
-		recipe_id INTEGER,
-		proportion REAL);')
-	c.execute("""SELECT fr.recipe_id,ingredient_id, amount,yield_quantity,description FROM (SELECT 
+	c.execute("""CREATE TEMP TABLE temptable AS SELECT 
+		fr.recipe_id,ingredient_id, amount,
+		yield_quantity,description,-1 AS idx
+		FROM (SELECT 
 		CASE
 		WHEN summary.total < 5 THEN 0
 		ELSE ingredients.ingredient_id
 		END AS ingredient_id,
 		recipe_id, description,amount
 		FROM ingredients,
-		(SELECT ingredient_id,count(ingredient_id) AS total FROM ingredients, 
-		(SELECT * FROM recipes WHERE search_query='hummus') r 
+		(SELECT ingredient_id,count(ingredient_id) AS total 
+		FROM ingredients, 
+		(SELECT * FROM recipes WHERE search_query='%s') r 
 		WHERE ingredients.recipe_id=r.recipe_id  
 		GROUP BY ingredient_id 
 		ORDER BY count(*)) summary
 		WHERE summary.ingredient_id = ingredients.ingredient_id) fr
-		INNER JOIN (SELECT recipe_id, yield_quantity FROM recipes) sr
-		ON fr.recipe_id = sr.recipe_id;""")
-
+		INNER JOIN (SELECT 
+		recipe_id,sum(amount) AS yield_quantity FROM ingredients
+		GROUP BY recipe_id) sr
+		ON fr.recipe_id = sr.recipe_id;""" % (query))
+	c.execute("""UPDATE temptable SET idx=
+		(SELECT count(*) FROM 
+		(SELECT DISTINCT ingredient_id AS ingredient_id
+		FROM temptable AS tt) tt2
+		WHERE temptable.ingredient_id > tt2.ingredient_id
+		);""")
+	c.execute("""SELECT * FROM temptable;""")
+	data = c.fetchall()
+	c.close()
+	conn.close()
 
 if __name__=='__main__':
 	main(sys.argv[1:])
