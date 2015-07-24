79039875863e70fb62ded67907b31eb333817f85
Max Candocia <mscandocia@gmail.com>
2015-07-24 12:31:57 -0500

    added many comments

--- analyze_data/linear_modprog_solution.py
+++ analyze_data/linear_modprog_solution.py
@@ -19,9 +19,13 @@ import copy
 #output this new table to python
 
 def main(args):
+	"""doesn't really need to exist, but can be modified if needed;
+	demonstrates how to use functions in context of analysis"""
+	return
 	query = 'hummus'
 	data = get_model_data(query)
 	data = apply_response_function(data)
+	data = require_ingredient_bounds(data,'chickpeas',[0.2,1])
 	xvar = data[0]
 	yvar = data[1]
 	#as long as no cuts are made to response value
@@ -29,7 +33,9 @@ def main(args):
 	model = LassoLarsIC(criterion='aic',fit_intercept = False)
 	model.fit(xvar,yvar)
 	params = model.coef_
-	
+	sol = solve_linear_programming(data[0],model)
+	v = sol.values()
+	print numpy.column_stack((data[3],v[4]))
 
 #may want to move function to other file later
 def read_data(query,cutoff = 5):
@@ -100,6 +106,8 @@ def read_data(query,cutoff = 5):
 
 #very rough solution
 def fix_ingredient_labels(labels):
+	"""a cheap hack that defines an ingredient's label based on
+	the shortest one with a shared recipe ID"""
 	descriptions = []
 	pv = -1
 	for label in labels:
@@ -109,6 +117,8 @@ def fix_ingredient_labels(labels):
 	return descriptions
 
 def extract_rating_data(query):
+	"""returns the recipe IDs, number of ratings, and average rating
+	of recipes from a query"""
 	conn = sqlite3.connect('recipes.db')
 	c = conn.cursor()
 	c.execute("""CREATE TEMP TABLE qtable AS
@@ -134,24 +144,38 @@ def extract_rating_data(query):
 	return data
 
 def apply_response_function(data):
+	"""wrapper to modify the y-value of `data`
+	with response_function"""
 	data[1] = response_function(data[1])
 	return data
 
 #objective function for recipes
 def response_function(y):
+	"""transforms number of ratings and total rating into a 
+	response value"""
 	return numpy.asarray([x[2]*math.log(1+x[1]) for x in y])
 
 def get_model_data(query,cutoff=5,other_ingredient_cutoff=0,new_column_cutoff=0):
+	"""Takes a query and  cutoff value for # of times an ingredient appears,
+	returns a length-4 dataset with
+	[xval,yval,xlabel,ylabel]
+	other_ingredient_cutoff can be used to limit the amount of unspecified
+	ingredients that go into a recipe"""
 	data = read_data(query,cutoff)
 	response = extract_rating_data(query)
 	return create_model_frame(data,response,query,other_ingredient_cutoff,new_column_cutoff)
 
 def keep_data_numbers(data):
+	"""Rearranges the numeric portion of the data to be used in
+	create_model_frame()"""
 	data = [[x[0],x[5],x[2]/x[3]] for x in data]
 	#data = numpy.asarray(data)
 	return data
 
 def create_model_frame(data,response,query,other_ingredient_cutoff = 0,new_column_cutoff=0):
+	"""takes in the SQL-fetched data and then rearranges it to a 
+	numpy data frame, as well as handling the response values,
+	in case additional cutoffs are made"""
 	ilabels = ['OTHER'] + data[1]
 	ilabels = numpy.asarray(ilabels)
 	rlabels = numpy.asarray(extract_recipe_labels(query))
@@ -180,6 +204,8 @@ def create_model_frame(data,response,query,other_ingredient_cutoff = 0,new_colum
 	return [new_array,response,rlabels,ilabels]
 
 def extract_recipe_from_url(url):
+	"""parses URL for recipe name;
+	may need to be updated for other websites beyond allrecipes.com"""
 	url = re.sub('^.*/Recipe/','',url)
 	url = re.sub('/.*$','',url)
 	url = re.sub('-','_',url)
@@ -188,6 +214,8 @@ def extract_recipe_from_url(url):
 #currently based on URLs
 #method may need to be updated if sites other than allrecipes.com are used
 def extract_recipe_labels(query):
+	"""Searches for all relevant recipe URLs for a given query, and then
+	it parses them to get the recipe names"""
 	conn = sqlite3.connect('recipes.db')
 	c = conn.cursor()
 	c.execute("""CREATE TEMP TABLE qtable AS
@@ -216,6 +244,10 @@ def extract_recipe_labels(query):
 #this function runs the linear programming solver
 #bounds is redudnantly defined as a matrix
 def solve_linear_programming(data,model):
+	"""This takes in either the length-4 list of data or the first element
+	of said list, as well as whatever model contains the coefficients,
+	and then it constructs the constraints and solves the linear
+	programming problem"""
 	if type(data)==type([1,2,3]):
 		print 'Taking data=data[0]'
 		data = data[0]
@@ -231,6 +263,11 @@ def solve_linear_programming(data,model):
 #these functions are used for constructing constraints
 
 def bounds_by_rank_kernel(x,lk,uk):
+	"""This function takes in a 1D array, and 2 python lists
+	with float values.  In order to create bounds, it uses these
+	lists as a kernel to smooth the "bounds" of what kind of values appear
+	in the data set.  Note that substitute ingredients (e.g., oil & butter)
+	need to be considered separately in a function not yet created."""
 	vals = numpy.sort(x)
 	rvals = -numpy.sort(-x)
 	lower = 0
@@ -243,6 +280,9 @@ def bounds_by_rank_kernel(x,lk,uk):
 
 def construct_bounds_constraints(x,lower_weights = [0.1,0.3,0.4,0.2],
 				 upper_weights=[0.1,0.3,0.4,0.2]):
+	"""This function takes in the data set and then applies
+	the bounds_by_rank_kernel to each variable, returning
+	a list of the [lower,upper] bounds for each variable."""
 	#get bounds
 	bounds = []
 	for i in range(x.shape[1]):
@@ -259,6 +299,8 @@ def construct_bounds_constraints(x,lower_weights = [0.1,0.3,0.4,0.2],
 	#	(lvals,uvals))]
 	
 def require_ingredient_bounds(data,ingredient_name,bounds):
+	"""This removes rows from the data that do not have an ingredient
+	fall into certain bounds.  Useful in absence of actual clustering"""
 	col_index = list(data[3]).index(ingredient_name)
 	keep_lower = data[0][:,col_index] >= bounds[0]
 	keep_upper = data[0][:,col_index] <= bounds[1]
@@ -274,6 +316,7 @@ def construct_substitute_ingredient_constraints(data,maxcor = -0.5):
 	pass
 
 def construct_unit_constraints(data):
+	"""Creates the constraint for all ingredients to add to 1"""
 	dims = data.shape
 	return [numpy.ones([1,dims[1]]),[1]]
 
